3322dc3b3dd23c2080cd3a5e9b1b17819386faf9
sofie1298 <sofialanper@gmail.com>
2024-02-26 19:48:17 -0600

    Examen Viviana Lanza

---
+++ app/Http/Controllers/VuelosAsientosController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class VuelosAsientosController extends Controller
+{
+    //
+    public function MenuPrincipal(){
+        return view('inicio');
+    }
+}
